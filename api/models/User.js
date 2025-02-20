99e3bc8945f5e8b10e3fe2b536ef538d5a7ba49b
Tarik Carey <renewed@gmail.com>
2025-02-20 17:54:23 -0500

    Finished setting up User Schema in User Model, Added bcrypt

--- api/models/User.js
+++ api/models/User.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
-const userSchema = mongoose.Schema(
+const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
@@ -28,7 +28,7 @@ const userSchema = mongoose.Schema(
     genderPreference: {
       type: String,
       required: true,
-      enum: ["Male", "Female", "Other"],
+      enum: ["Male", "Female", "both"],
     },
     bio: {
       type: String,
@@ -53,3 +53,20 @@ const userSchema = mongoose.Schema(
     },
     },
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+}
+);
+
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+}
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
