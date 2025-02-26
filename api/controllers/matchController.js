803ebbe96a42219c59eec69ca8a2f5120f9a1fe9
Tarik Carey <renewed@gmail.com>
2025-02-26 12:24:46 -0700

    Update matchController
    Setup GetMatches

--- api/controllers/matchController.js
+++ api/controllers/matchController.js
@@ -1,4 +1,28 @@
+import User from "../models/User.js";
 export const swipeRight = async (req, res) => { };
 export const swipeLeft = async (req, res) => { };
-export const getMatches = async (req, res) => { };
+
+
+export const getMatches = async (req, res) => { 
+
+try{
+    const user = await User.findById(req.user._id).populate ("matches", "name image");
+
+res.status(200).json({
+    success: true,
+    matches: user.matches,
+});
+
+} catch (error) {
+    console.log("Error in getMatches",error);
+
+    res.status(500).json({ 
+        success: false,
+        message: "Error getting matches - Internal Server Error",
+        error: error.message });
+}
+
+};
+
+
 export const getUserProfiles = async (req, res) => { };
