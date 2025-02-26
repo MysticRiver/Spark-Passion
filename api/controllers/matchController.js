b770d5a625582545e23b7c1b7084bcdb644cfc44
Tarik Carey <renewed@gmail.com>
2025-02-26 14:46:05 -0700

    Set up getUserProfiles function

--- api/controllers/matchController.js
+++ api/controllers/matchController.js
@@ -26,37 +26,35 @@ res.status(200).json({
 
 
 export const getUserProfiles = async (req, res) => {
+	try {
+		const currentUser = await User.findById(req.user.id);
 
-try{
-    const currentUser = await User.findById(req.user.id);
-  const users = await User.find({
-    $and:   [
-        { _id: { $ne: currentUser._id } },
-        { _id: { $nin: currentUser.likes } },
-        { _id: { $nin: currentUser.dislikes } },
-        { _id: { $nin: currentUser.matches } },
-        {
-            gender: {
-                currentUser.genderPreference, === "both"
-                ? { $in: ["male", "female"] }
-                : currentUser.genderPreference,
-            },
-        }
+		const users = await User.find({
+			$and: [
+				{ _id: { $ne: currentUser.id } },
+				{ _id: { $nin: currentUser.likes } },
+				{ _id: { $nin: currentUser.dislikes } },
+				{ _id: { $nin: currentUser.matches } },
+				{
+					gender:
+						currentUser.genderPreference === "both"
+							? { $in: ["male", "female"] }
+							: currentUser.genderPreference,
+				},
+				{ genderPreference: { $in: [currentUser.gender, "both"] } },
+			],
+		});
 
-    ],
-})
-    
+		res.status(200).json({
+			success: true,
+			users,
+		});
+	} catch (error) {
+		console.log("Error in getUserProfiles: ", error);
 
-    
-
-} catch (error) {
-    console.log("Error in getMatches", error);
-
-    res.status(500).json({
-        success: false,
-        message: "Error getting matches - Internal Server Error",
-        error: error.message });
-}
-
-
- };
+		res.status(500).json({
+			success: false,
+			message: "Internal server error",
+		});
+	}
+};
