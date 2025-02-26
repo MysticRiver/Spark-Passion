d7ad2235c708855a26adbb61afcc6a6d836e348a
Tarik Carey <renewed@gmail.com>
2025-02-25 20:14:50 -0700

    Beginning setup User Controller

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,5 +1,9 @@
 import express from "express";
-
+import { protectRoute } from "../middleware/auth.js";
 const router = express.Router();
+
+router.put("/update", protectRoute, (req, res) => {
+    res.send("update user");
+});
 
 export default router;
