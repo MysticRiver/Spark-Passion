2b5ab94519d5aa624ee1dc04e2d9c776527d5d3f
Tarik Carey <renewed@gmail.com>
2025-02-26 12:11:18 -0700

    Match Routes and Controller
    Initial setup of Match Route and Controller

---
+++ api/controllers/matchController.js
@@ -0,0 +1,4 @@
+export const swipeRight = async (req, res) => { };
+export const swipeLeft = async (req, res) => { };
+export const getMatches = async (req, res) => { };
+export const getUserProfiles = async (req, res) => { };
