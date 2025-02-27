e10c4614e163596bf8a4dafccd08949335297182
Tarik Carey <renewed@gmail.com>
2025-02-27 08:46:00 -0700

    update matchController

--- api/controllers/matchController.js
+++ api/controllers/matchController.js
@@ -1,5 +1,43 @@
 import User from "../models/User.js";
-export const swipeRight = async (req, res) => { };
+export const swipeRight = async (req, res) => { 
+    try{
+        const {likedUserId} = req.params;
+        const currentUser = await User.findById(req.user.id);
+        const likedUser = await User.findById(likedUserId);
+
+        if(!likedUser){
+            return res.status(404).json({
+                success: false,
+                message: "User not found",
+            });
+        }
+
+        if(!currentUser.likes.includes(likedUserId)){
+            currentUser.matches.push(userId);
+            otherUser.matches.push(req.user._id);
+            await currentUser.save();
+            await otherUser.save();
+            return res.status(200).json({
+                success: true,
+                message: "It's a match!",
+            });
+        }
+
+        currentUser.likes.push(userId);
+        await currentUser.save();
+        return res.status(200).json({
+            success: true,
+            message: "User liked successfully",
+        });
+    } catch (error) {
+        console.log("Error in swipeRight", error);
+        res.status(500).json({
+            success: false,
+            message: "Error in swipeRight - Internal Server Error",
+            error: error.message,
+        });
+    }
+};
 export const swipeLeft = async (req, res) => { };
 
 
