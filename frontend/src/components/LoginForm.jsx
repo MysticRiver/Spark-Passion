d383e161f3a84a4cb6be9f7a43ab24ba0ab0e5e0
Tarik Carey <renewed@gmail.com>
2025-03-01 19:20:35 -0700

    AuthPage

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { useAuthStore } from "../store/useAuthStore";
 
 const LoginForm = () => {
     const {email, setEmail} = useState('');
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -19,12 +19,25 @@ export const useAuthStore = create((set) => ({
         set({loading: false});
     }
   },
+  logout: async () => {
+    try {
+        const res = await axiosInstance.post("/auth/logout");
+        disconnectSocket();
+        if (res.status === 200) set({ authUser: null });
+    } catch (error) {
+        toast.error(error.response.data.message || "Something went wrong");
+    }
+},
   checkAuth: async () => {
     try {
         const res = await axiosInstance.get("/auth/me");
-        console.log(res.data);
+        initializeSocket(res.data.user._id);
+        set({ authUser: res.data.user });
     } catch (error) {
+        set({ authUser: null });
         console.log(error);
+    } finally {
+        set({ checkingAuth: false });
     }
-  }
+},
 }));
