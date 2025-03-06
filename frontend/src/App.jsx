83d29821f1f622902bd4794589682dd4f06cd0ad
Tarik Carey <renewed@gmail.com>
2025-03-06 14:26:01 -0700

    Set up React Toaster component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,6 +24,7 @@ function App() {
 				<Route path='/profile' element={<ProfilePage />} />
         <Route path='/chat/:id' element={<MessagePage />} />
 			</Routes>
+      <Toaster /> 
     </div>
      
     </>
