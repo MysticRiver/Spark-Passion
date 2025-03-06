43159023867117079b9ce1b929bd0aa41a1c1158
Tarik Carey <renewed@gmail.com>
2025-03-06 12:12:37 -0700

    LoginForm created

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -4,7 +4,7 @@ import LoginForm from '../components/LoginForm'
 import SignUpForm from '../components/SignUpForm'
 
 const AuthPage = () => {
-  const{isLogin, setIsLogin} = useState(true);
+  const [isLogin, setIsLogin] = useState(true);
   return (
     <div
 			className='min-h-screen flex items-center justify-center bg-gradient-to-br
